66051131572775388b0b155a9190a642280d1b2c
JeffreyTerry <jterry94@gmail.com>
2014-08-31 22:09:48 -0500

    added map view

--- src/com/example/trackme/MainActivity.java
+++ src/com/example/trackme/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.trackme;
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -10,12 +11,15 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
-	private static final long MINIMUM_DISTANCECHANGE_FOR_UPDATE = 1; // in
+public class MainActivity extends ActionBarActivity implements OnClickListener {
+	private static final long MINIMUM_DISTANCECHANGE_FOR_UPDATE = 5; // in
 																		// Meters
-	private static final long MINIMUM_TIME_BETWEEN_UPDATE = 1000; // in
+	private static final long MINIMUM_TIME_BETWEEN_UPDATE = 2000; // in
 																	// Millisecond
 
 	ArrayList<Long> locationChangeTimes;
@@ -34,6 +38,8 @@ public class MainActivity extends ActionBarActivity {
 
 		tvMain = (TextView) findViewById(R.id.tvMain);
 		tvLocation = (TextView) findViewById(R.id.tvLocation);
+		Button bViewMap = (Button) findViewById(R.id.bViewMap);
+		bViewMap.setOnClickListener(this);
 
 		locationChangeTimes = new ArrayList<Long>();
 		locationLatitudes = new ArrayList<Double>();
@@ -174,6 +180,40 @@ public class MainActivity extends ActionBarActivity {
 
 		}
 
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.bViewMap:
+			try {
+				Class mapClass = Class
+						.forName("com.example.trackme.MapActivity");
+				Intent ourIntent = new Intent(this, mapClass);
+
+				Bundle extras = new Bundle();
+				if (locationLatitudes.size() > 0) {
+					double latitudes[] = new double[locationLatitudes.size()];
+					double longitudes[] = new double[locationLongitudes.size()];
+					for (int i = 0; i < latitudes.length; i++) {
+						latitudes[i] = locationLatitudes.get(i);
+						longitudes[i] = locationLongitudes.get(i);
+					}
+					extras.putDoubleArray("latitudes", latitudes);
+					extras.putDoubleArray("longitudes", longitudes);
+					ourIntent.putExtras(extras);
+				} else {
+					extras.putDoubleArray("latitudes", new double[] {});
+					extras.putDoubleArray("longitudes", new double[] {});
+					ourIntent.putExtras(extras);
+				}
+
+				startActivity(ourIntent);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			break;
+		}
 	}
 
 }
